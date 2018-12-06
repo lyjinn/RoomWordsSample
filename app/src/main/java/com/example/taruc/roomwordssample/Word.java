eebc5d87fed49b584ee237a877452bf4e7594ce2
anreh <anurehka@gmail.com>
2018-12-06 12:57:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/roomwordssample/Word.java
@@ -0,0 +1,17 @@
+package com.example.taruc.roomwordssample;
+
+import android.arch.persistence.room.*;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
